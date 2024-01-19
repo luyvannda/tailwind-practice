da46baae88ae15c164fb9dbed4437fdd8ff09427
luyvannda <philips.joe329@gmail.com>
2024-01-19 13:06:58 +0700

    add prettier to format tailwind before commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl italic">Title 1</h1>
-      <h2 className="text-2xl">Title 2</h2>
-      <h3 className="text-xl">Title 3</h3>
-      <p className="text-base underline decoration-red-300 decoration-wavy underline-offset-4 leading-normal">A regular paragraph</p>
-      <p className="text-sm">A description paragraph</p>
-      <p className="note text-xs">A little note</p>
+    <main className="h-screen flex">
+      <div className="h-12 w-1/3 bg-green-500 text-center text-white">Hello
+      </div>
+      <div className="h-12 w-1/3 bg-red-500 text-center">My
+      </div>
+      <div className="h-12 w-1/3 bg-blue-500 text-center">Friend
+      </div>
     </main>
   )
 }
