6325b0a46661faf25520d70c604098d923ea85ad
luyvannda <philips.joe329@gmail.com>
2024-01-19 11:45:34 +0700

    learn to import font and set custom font-family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold border-4 border-green-500">Hello World</h1>
-      <h1 className="text-black sm:text-dragon">Here my custom color</h1>
+      <h1 className="text-3xl italic">Title 1</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Title 3</h3>
+      <p className="text-base underline decoration-red-300 decoration-wavy underline-offset-4 leading-normal">A regular paragraph</p>
+      <p className="text-sm">A description paragraph</p>
+      <p className="note text-xs">A little note</p>
     </main>
   )
 }
