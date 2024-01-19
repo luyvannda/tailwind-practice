0c7a53a7e284e9e3228e5dae0c1012e4bc10ac9f
luyvannda <philips.joe329@gmail.com>
2024-01-19 14:45:44 +0700

    learn to use tailwind spaces and sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="h-screen flex">
-      <div className="h-12 w-1/3 bg-green-500 text-center text-white">Hello
+    <main className="flex min-h-screen space-x-4">
+      <div className="w-1/5 bg-green-500 text-center text-white">Side Bar
       </div>
-      <div className="h-12 w-1/3 bg-red-500 text-center">My
+      <div className="w-4/5 bg-red-500 text-center">Main Content
       </div>
-      <div className="h-12 w-1/3 bg-blue-500 text-center">Friend
-      </div>
+
     </main>
   )
 }
