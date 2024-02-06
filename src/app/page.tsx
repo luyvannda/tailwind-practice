87a2607b4fc8828f8b001c1819f36f4cb96ee6a0
luyvannda <luyvannda@gmail.com>
2024-02-06 15:40:06 +0700

    practice group, peer and order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 import Image from 'next/image';
 export default function Home() {
   return (
-    <div>
-      <main className="m-4 sm:flex sm:flex-row sm:items-center">
-        <Image
-          src="https://cdn.wallpapersafari.com/21/76/v26KTO.jpg"
-          alt="RDJ"
-          priority={true}
-          width={400}
-          height={500}
-          className="rounded-2xl transition-all duration-200 hover:scale-110 sm:h-1/2 sm:w-1/2"
-        />
-        <section className="sm:mx-5">
-          <p className="my-3 font-bold md:text-lg lg:text-2xl dark:text-white">
-            Robert Downey Jr. is an American actor and producer. His career has
-            been characterized by critical and popular success in his youth,
-            followed by a period of substance abuse and legal troubles, before a
-            resurgence of commercial success later in his career.
-          </p>
-          <button className="rounded-md bg-black px-4 py-2 font-bold text-yellow-300 ring-green-500 transition-all duration-300 sm:text-lg sm:hover:bg-yellow-300 sm:hover:text-black sm:focus:ring-2 sm:active:bg-yellow-500 lg:text-2xl dark:bg-yellow-300 dark:text-black">
-            Visit Instagram
-          </button>
-        </section>
-      </main>
+    <div className="group border-2 bg-white p-6 hover:bg-sky-500">
+      <p className="text-slate-500 group-hover:text-pink-800">
+        Create a new project from a variety of starting templates
+      </p>
+      <p className="text-slate-500">
+        Create a new project from a variety of starting templates
+      </p>
+      <p className="text-slate-500 group-hover:text-3xl">
+        Create a new project from a variety of starting templates
+      </p>
+
+      <div className="flex flex-col bg-purple-400">
+        <div className="peer/yellowSquare order-4 h-12 w-12 bg-yellow-500"></div>
+
+        <div className="peer/blackSquare order-3 h-12 w-12 bg-black"></div>
+
+        <div className="order-1 hidden peer-hover/yellowSquare:block">
+          You are hovering the yellow square
+        </div>
+        <div className="order-2 hidden peer-hover/blackSquare:block">
+          You are hovering the black square
+        </div>
+      </div>
     </div>
   );
 }
